c67a3a748d1b8749242723a4ecf0b76efc265473
akshatjagga <Akshat@akshats-macbook-pro.local>
2018-05-28 20:04:57 +0530

    Initial Commit

---
+++ Nosco/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Nosco
+//
+//  Created by Akshat Jagga on 28/05/18.
+//  Copyright © 2018 Akshat Jagga. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
